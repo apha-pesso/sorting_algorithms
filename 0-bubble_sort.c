fecd9cd82d340b031d48020f3b8086d58f2644da
apha-pesso <mutiuayo99@gmail.com>
2022-10-12 11:10:18 +0000

    check

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,8 +1,9 @@
 #include "sort.h"
 
 /**
- * swap- finction t swap 2 elements
- * @a: first element to be swapped
+ * swap - switch the positions of elements passed
+ *
+ * @a: pointer to the first element
  * @b: pointer to the second element
  */
 void swap(int *a, int *b)
@@ -15,30 +16,31 @@ void swap(int *a, int *b)
 }
 
 /**
-* bubble_sort- Function to sort array in ascending order
-* @array: The array to be sorted
-* @size: The size of the array to be sorted
-* Return: Return 0
-*/
+ * bubble_sort - sorts an array of integers in ascending order
+ *              using the Bubble sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ *
+ */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0, action;
+	size_t i, j, flag;
 
 	if (size >= 2)
 	{
 		for (i = 0; i < size - 1; i++)
 		{
-			action = 0;
+			flag = 0;
 			for (j = 0; j < size - i - 1; j++)
 			{
 				if (array[j] > array[j + 1])
 				{
-					swap(&(array[j]), &(array[j + 1]));
-					action = 1;
+					swap(&array[j], &array[j + 1]);
+					print_array(array, size);
+					flag = 1;
 				}
-				print_array(array, size);
 			}
-			if (action == 0)
+			if (flag == 0)
 				break;
 		}
 	}
